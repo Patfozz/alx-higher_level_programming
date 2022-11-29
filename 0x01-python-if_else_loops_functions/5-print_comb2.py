e4049026c4ded6374f7faaf191ad2859f72695ed
Patfozz <patfoskypin@gmail.com>
2022-11-29 11:57:34 -0800

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print(i, end='')
+for i in range(00, 100):
     if i != 99:
-        print(",", end='')
+        print("{:02d}".format(i), end=',')
+    elif i == 99:
+        print("{:02d}".format(i))
